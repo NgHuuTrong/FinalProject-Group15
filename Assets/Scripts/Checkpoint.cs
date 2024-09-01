218507b900f8708ebee5607419e56c90e580cc52
DucHungGithub <hungnp12345@gmail.com>
2024-09-01 16:48:38 +0700

    Effect light

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+
+    public GameObject cpOn, cpOff;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (cpOff != null)
+            cpOff.SetActive(true);
+        if (cpOn != null)
+            cpOn.SetActive(false);
     }
 
     // Update is called once per frame
@@ -20,6 +26,13 @@ public class Checkpoint : MonoBehaviour
     private void OnTriggerEnter(Collider other) {
         if (other.tag == "Player") {
             GameManager.instance.SetSpawnPoint(transform.position);
+            if (cpOff != null) {
+                cpOff.SetActive(false);
+            }
+            if (cpOn != null) {
+                cpOn.SetActive(true);
+            }
         }
     }
+
 }
