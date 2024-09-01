1b1ae670a3be2c7c976c93820691ac933638208c
DucHungGithub <hungnp12345@gmail.com>
2024-09-02 00:10:57 +0700

    health system

---
+++ Assets/Scripts/HealthManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+    public static HealthManager instance;
+
+    public int currentHealth, maxHealth;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Hurt()
+    {
+        currentHealth--;
+        if(currentHealth <= 0)
+        {
+            currentHealth = 0;
+            GameManager.instance.Respawn();
+        }
+        else
+        {
+            PlayerController.instance.knockback();
+        }
+    }
+
+    public void rsetHealth()
+    {
+        currentHealth = maxHealth;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,6 +16,12 @@ public class PlayerController : MonoBehaviour
     public GameObject playerModal;
     public float rotateSpeed;
     public Animator anim;
+
+    public bool isKnocking;
+    public float knockBackLength = 0.5f;
+    private float knockbackCounter;
+    public Vector2 knockbackPower;
+
     private void Awake() {
         instance = this;
     }
@@ -28,29 +34,70 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float yStore = moveDirection.y;
-        moveDirection = transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal");
-        moveDirection.Normalize();
-        moveDirection *= moveSpeed;
-        moveDirection.y = yStore;
+        if(!isKnocking)
+        {
+            float yStore = moveDirection.y;
+            moveDirection = transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal");
+            moveDirection.Normalize();
+            moveDirection *= moveSpeed;
+            moveDirection.y = yStore;
 
-        if (characterController.isGrounded) {
-            moveDirection.y = -1f;
-            if (Input.GetButtonDown("Jump")) {
-                moveDirection.y = jumpForce;
+            if (characterController.isGrounded)
+            {
+                moveDirection.y = -1f;
+                if (Input.GetButtonDown("Jump"))
+                {
+                    moveDirection.y = jumpForce;
+                }
+            }
+            moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+
+            characterController.Move(moveDirection * Time.deltaTime);
+
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+            {
+                transform.rotation = Quaternion.Euler(0f, theCam.transform.rotation.eulerAngles.y, 0f);
+                Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+                playerModal.transform.rotation = Quaternion.Slerp(playerModal.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
             }
         }
-        moveDirection.y  += Physics.gravity.y * Time.deltaTime * gravityScale;
 
-        characterController.Move(moveDirection * Time.deltaTime);
+        if(isKnocking)
+        {
+            knockbackCounter -= Time.deltaTime;
 
-        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0) {
-            transform.rotation = Quaternion.Euler(0f, theCam.transform.rotation.eulerAngles.y, 0f);
-            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
-            playerModal.transform.rotation =  Quaternion.Slerp(playerModal.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+            float yStore = moveDirection.y;
+            moveDirection = playerModal.transform.forward * -knockbackPower.x;
+            moveDirection.y = yStore;
+
+            if (characterController.isGrounded)
+            {
+                moveDirection.y = -1f;
+                
+            }
+
+            moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+
+
+            characterController.Move(moveDirection * Time.deltaTime);
+            
+
+            if (knockbackCounter <= 0)
+            {
+                isKnocking = false;
+            }
         }
 
         anim.SetFloat("Speed", Math.Abs(moveDirection.x) + Math.Abs(moveDirection.z));
         anim.SetBool("Grounded", characterController.isGrounded);
+    }
+
+    public void knockback()
+    {
+        isKnocking = true;
+        knockbackCounter = knockBackLength;
+        Debug.Log("Knock back");
+        moveDirection.y = knockbackPower.y;
+        characterController.Move(moveDirection * Time.deltaTime);
     }
 }
